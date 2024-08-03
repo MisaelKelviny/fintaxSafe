8d2a5b500bb846dd3d1526777e6e048664300825
KelvinyMisael <misaelkelviny@gmail.com>
2024-08-03 00:14:43 -0300

    adjusting images

--- src/data/constants/benefits.ts
+++ src/data/constants/benefits.ts
@@ -1,25 +1,25 @@
 export const benefits = [
   {
-    img: "/nextjs-github-pages/image/bg4.png",
+    img: "/fintaxSafe/image/bg4.png",
     title: "Segurança",
     text: `O certificado digital permite que qualquer tipo de documento eletrônico seja assinado com valor jurídico, sem
     que o mesmo corra o risco de ser fraudado ou alterado.`,
     reverse: false,
   },
   {
-    img: "/nextjs-github-pages/image/bg3.png",
+    img: "/fintaxSafe/image/bg3.png",
     title: "Praticidade",
     text: `Você pode fazer tudo isso sem sair da empresa o que gera praticidade é a possibilidade de armazenar arquivos na nuvem com o máximo de segurança.`,
     reverse: true,
   },
   {
-    img: "/nextjs-github-pages/image/bg1.png",
+    img: "/fintaxSafe/image/bg1.png",
     title: "Redução de Custo",
     text: `A partir do momento em que você começa a utilizar o certificado digital, você não precisa mais se preocupar com a impressão de documentos, gastos com reconhecimento de firma em cartórios e transporte para o envio de arquivo.`,
     reverse: false,
   },
   {
-    img: "/nextjs-github-pages/image/bg2.png",
+    img: "/fintaxSafe/image/bg2.png",
     title: "Sustentabilidade",
     text: `Você consegue tornar a sua empresa muito mais sustentável, já que a emissão das notas fiscais eletrônicas, por exemplo, é feita virtualmente, dispensando a utilização de papel.`,
     reverse: true,
