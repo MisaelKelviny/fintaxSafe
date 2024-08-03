f72da56dcb0be212b2b020382adcbb3df7a5457b
KelvinyMisael <misaelkelviny@gmail.com>
2024-08-02 23:45:39 -0300

    fixing build

--- src/components/LandingPage/Modal/index.tsx
+++ src/components/LandingPage/Modal/index.tsx
@@ -1,44 +1,49 @@
-import Modal from 'react-modal';
-import styles from './styles.module.css';
+import Modal from "react-modal";
+import styles from "./styles.module.css";
 
 const customStyles = {
   content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
   },
   overlay: {
-    background: 'rgb(46 46 45 / 57%)'
-  }
+    background: "rgb(46 46 45 / 57%)",
+  },
 };
 
-Modal.setAppElement('body');
+Modal.setAppElement("body");
 
 type ModalProp = {
-  children: any,
-  open: boolean,
-  setOpen: (value: boolean) => void
-}
+  children: any;
+  open: boolean;
+  setOpen: (value: boolean) => void;
+};
 
 export default function ModalSimple({ children, open, setOpen }: ModalProp) {
   return (
-    <div style={{ position: 'relative' }}>
+    <div style={{ position: "relative" }}>
       <Modal
         isOpen={open}
         onRequestClose={() => setOpen(false)}
         style={customStyles}
         shouldCloseOnOverlayClick={false}
       >
-        <div className={styles.closeContainer}>
-          <button onClick={() => setOpen(false)} className={styles.closeButton}>X</button>
-        </div>
-        <div>
-          {children}
-        </div>
+        <>
+          <div className={styles.closeContainer}>
+            <button
+              onClick={() => setOpen(false)}
+              className={styles.closeButton}
+            >
+              X
+            </button>
+          </div>
+          <div>{children}</div>
+        </>
       </Modal>
     </div>
-  )
+  );
 }
